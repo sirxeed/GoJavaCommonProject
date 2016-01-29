787f5b6e70e4b099ff29fb42803ad625d25b98c1
Max <xpro100bestx@gmail.com>
2016-01-29 23:43:18 +0200

    -added precisionInput; -improved angleValue;

--- src/UserInputUtils.java
+++ src/UserInputUtils.java
@@ -5,7 +5,7 @@ public class UserInputUtils {
 
     public boolean angleInputValidate(String angle) {
         boolean checker = false;
-        char[] chars = convertStringToCharArray(angle);
+        char[] chars = angle.toCharArray();
 
         //Let's check if user typed a dimension
         if (chars[chars.length - 1] == 'd' || chars[chars.length - 1] == 'r') {
@@ -19,17 +19,13 @@ public class UserInputUtils {
 
     public double angleValue(String angle) {
         float angleFloat = 0;
-        char[] chars = convertStringToCharArray(angle);
+        char[] chars = angle.toCharArray();
         char type = chars[chars.length - 1];
         //If 'yes' let's check if we can parse it to float
-        char[] newChars = new char[chars.length - 1];
-        for (int i = 0; i < newChars.length; i++) {
-            newChars[i] = chars[i];
-        }
 
         StringBuilder angleNumbers = new StringBuilder();
-        for (char newChar : newChars) {
-            angleNumbers.append(newChar);
+        for (int i = 0; i < chars.length - 1; i++) {
+            angleNumbers.append(chars[i]);
         }
         try {
             angleFloat = Float.parseFloat(angleNumbers.toString());
@@ -44,12 +40,32 @@ public class UserInputUtils {
         return angleFloat;
     }
 
-    public static char[] convertStringToCharArray(String angle) {
-        return angle.toCharArray();
-    }
-
     public static double angleIntoRadians(float angle) {
         return (angle * Math.PI) / 180;
+    }
+
+    public float precisionInput(String userInput) {
+        float precision = 0;
+        char[] chars = userInput.toCharArray();
+        if (chars[chars.length - 1] == '1') {
+            //let's check if it was entered correctly.
+                for (int i = 0; i < chars.length - 1; i++) {
+                    if (i == 1) {
+                        if (chars[1] == '.') {
+                            continue;
+                        }
+                    } else {
+                        if (chars[i] != '0') {
+                            System.out.println("Wrong precision input. (i.e. 0.0001)");
+                            return 0;
+                        }
+                    }
+                }
+                precision = Float.parseFloat(userInput);
+        } else {
+            System.out.println("Wrong precision input. (i.e. 0.0001)");
+        }
+        return precision;
     }
 
 }
